edee2846ad10ec2fc2dcf0a9eca2e73fc609adf5
Oles Boiaryn <>
2024-04-08 21:38:04 +0200

    git add vocab files & set relative path

--- Program.cs
+++ Program.cs
@@ -6,10 +6,11 @@ class Program
         // Init variables
         var testText = "As of November 30 , 2015 , $ 151.8 million of the originated loans were sold into a securitization trust but not settled and thus were included as receivables , net . Notes and Other Debts Payable In November 2013 , the Rialto segment originally issued $ 250 million aggregate principal amount of the 7.00 % senior notes due 2018 ( \" 7.00 % Senior Notes \" ) , at a price of 100 % in a private placement .";
         int maxSequenceLength = 200;
-        var modelPath = "/Users/olesboiaryn/Downloads/distilbert-base-uncased_ner_finetuned_onnx/model.onnx";
-        var vocabPath = "/Users/olesboiaryn/Downloads/distilbert_tokenizer-vocab.txt";
-        var labelsPath = "/Users/olesboiaryn/Downloads/distilbert-base-uncased_ner_finetuned_onnx/labels_finer-139_top_rows_10000_top_labels_4.txt";
-
+        var filesDir = "distilbert-base-uncased_ner_finetuned_onnx";
+        var modelPath = Path.Combine(Environment.CurrentDirectory, filesDir, "model.onnx");
+        var vocabPath = Path.Combine(Environment.CurrentDirectory, filesDir, "distilbert_tokenizer-vocab.txt");
+        var labelsPath = Path.Combine(Environment.CurrentDirectory, filesDir, "labels_finer-139_top_rows_10000_top_labels_4.txt");
+        
         var distilBertConfig = new DistilBertConfig(maxSequenceLength, modelPath, vocabPath, labelsPath);
         var distilBert = new DistilBert(distilBertConfig);
 
